3a0b92d3348e3d7b64fa5347927a592ed0b9b3ed
Hernan Rodriguez <hernan.rod.98@gmail.com>
2021-03-15 19:18:41 -0300

    Agregado de librerias RTC y OLED
    Para poder trabajar correctamente con el AtTiny85 debi adaptar librerias existentes del RTC DS3231 y del Display OLED debido a que las que habia encontrado consumian mucha memoria del microcontrolador. Con las librerias desarrolladas se redujo el consumo de memoria a la mitad del consumo previo sin dichas librerias.
    
    Actualmente el codigo es un reloj digital con 2 alarmas configuradas que activan un led por un periodo breve de tiempo. La alarma 1 se activa una vez por minuto en el segundo 30 de cada minuto. La alarma 2 se activa cada vez que se cambia de hora (en el minuto 0 de cada hora).
    
    La deteccion del flanco de la alarma se logra mediante una interrupcion implementada en el pin 2 del AtTiny85 (PB3). En ese mismo pin se encuentra configurada internamente la resistencia de pull up.

--- main.ino
+++ main.ino
@@ -1,11 +1,17 @@
-#include <TinyWireM.h>
-#include <TinyOzOLED.h>
+#include <TinyRTC.h>
+#include <TinyOLED.h>
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define INTERRUPT_PIN PCINT3 
+#define INT_PIN PB3           
+#define PCINT_VECTOR PCINT0_vect      
 
 // inputs - outputs
 #define ADC   3
 #define LED   4
 #define PULS  5
-
 
 enum FlagEncoder{ ENC_STATIC, ENC_RIGHT, ENC_LEFT };
 FlagEncoder flag_encoder = ENC_STATIC;
@@ -13,36 +19,59 @@ FlagEncoder flag_encoder = ENC_STATIC;
 enum StateEncoder { ST_A, ST_B, ST_C, ST_D, ST_E, ST_F, ST_G };
 StateEncoder st_encoder = ST_A;
 
-void setup() {       
-  OzOled.init();
-  OzOled.printString("REGADOR");
-  OzOled.setCursorXY(0x00, 0x01);
-  OzOled.printString("AUTOMATICO!");
-  /*
-  pinMode(ADC, INPUT);         
-  pinMode(PULS, INPUT);      // pulsador del encoder
-  digitalWrite(PULS, HIGH);  // pull up interno
-  pinMode(LED, OUTPUT);     // LED indicador */
+bool flag_int = false;
+
+void setup() {   
+
+  cli();                            // Disable interrupts during setup
+  PCMSK |= (1 << INTERRUPT_PIN);    // Enable interrupt handler (ISR) for our chosen interrupt pin (PCINT1/PB1/pin 6)
+  GIMSK |= (1 << PCIE);             // Enable PCINT interrupt in the general interrupt mask
+  pinMode(INT_PIN, INPUT_PULLUP);   // Set our interrupt pin as input with a pullup to keep it stable
+  sei();                            //last line of setup - enable interrupts after setup
+  
+  TinyOled.init();
+
+  TinyRtc.initAlarm(ALARM_1);
+  TinyRtc.initAlarm(ALARM_2);
+  TinyRtc.setAlarm(ALARM_1, 30, 40, 12, 1);
+  TinyRtc.setAlarm(ALARM_2,  0,  0, 12, 1);
+  TinyRtc.customAlarm(ALARM_1, ALARM_SEC_MATCH);
+  TinyRtc.customAlarm(ALARM_2, ALARM_MIN_MATCH);
+  
+  pinMode(LED, OUTPUT);     // LED indicador 
+
+}
+
+// Interrupcion
+ISR(PCINT_VECTOR){
+  if( digitalRead(INT_PIN) == LOW ) {
+    digitalWrite(LED, HIGH);
+    flag_int = true;
+    TinyRtc.clearAlarm(ALARM_1);
+    TinyRtc.clearAlarm(ALARM_2);
+  }
 }
 
 void loop() {  
-  
-  //ME_Encoder();
-  /*
-  int adc = ReadADC();
-  if(LEVEL_2){
-    digitalWrite(LED, HIGH); 
-  } else
-    digitalWrite(LED, LOW); 
-  */
-  /*
-  if(flag_encoder == ENC_RIGHT){
-    digitalWrite(LED, HIGH);  
+  if(flag_int){
     delay(1000);
-  } else if(flag_encoder == ENC_LEFT){
-    digitalWrite(LED, HIGH);
-    delay(5000);
-  } else {
     digitalWrite(LED, LOW);
-  }*/
+    flag_int = false;
+  }
+
+  TinyOled.setCursorXY(0x00,0x00); 
+  TinyOled.printNumber(TinyRtc.getHours());
+  TinyOled.printChar(':');
+  TinyOled.printNumber(TinyRtc.getMinutes());
+  TinyOled.printChar(':');
+  TinyOled.printNumber(TinyRtc.getSeconds());
+
+  TinyOled.setCursorXY(0x00,0x01); 
+  TinyOled.printNumber(TinyRtc.getDayMonth());
+  TinyOled.printChar('/');
+  TinyOled.printNumber(TinyRtc.getMonth());
+  TinyOled.printChar('/');
+  TinyOled.printNumber(TinyRtc.getYear());  
+
+  delay(1000);
 }
